2254fe0cac90b4f5c2f3845594c534f8f3496a68
darthpbal <Ronquillo.paul@gmail.com>
2017-05-31 20:23:14 -0700

    added configuration file.

---
+++ parts/venusGps/tests/configureGps.ino
@@ -0,0 +1,24 @@
+//purpose of this program is to be able to detect the
+//end of message flag and terminate the line with a newline
+
+#include <SoftwareSerial.h>
+SoftwareSerial venusGps(10,11); //rx tx
+
+char prevChar = '\0';
+
+void setup() {
+    Serial.begin(9600);
+    venusGps.begin(9600);
+}
+
+
+
+void loop() {
+    if(venusGps.available()){
+        char currentChar = venus.read();
+        Serial.print(currentChar, HEX);
+        if(currentChar == 0x0A && prevChar == 0x0D) Serial.print('\n');
+        else Serial.print(' ');
+        prevChar = currentChar;
+    }
+}
