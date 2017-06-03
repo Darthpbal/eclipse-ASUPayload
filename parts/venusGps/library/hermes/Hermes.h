fa281f51597fe146063e032f11106ac202473d76
darthpbal <Ronquillo.paul@gmail.com>
2017-06-03 02:03:38 -0700

    formatting

--- parts/venusGps/library/hermes/Hermes.h
+++ parts/venusGps/library/hermes/Hermes.h
@@ -42,10 +42,10 @@ private:
     byte payload[payloadSize];
     byte response[responseSize];
     unsigned int payloadLength;
-    void mapMsgToPayloadLength();   //for setting the payload length to the appropriate value for all messages I'll implement
-    unsigned int calcChecksum();    //for calculating the checksum
-    bool readResponse();            //reads the ACk response of the GPS
-    bool detectFlag(char *flag);    //for listening on a serial port and returning true when a sequence is seen
+    void mapMsgToPayloadLength();               //for setting the payload length to the appropriate value for all messages I'll implement
+    unsigned int calcChecksum();                //for calculating the checksum
+    bool readResponse();                        //reads the ACk response of the GPS
+    bool detectFlag(char *flag);                //for listening on a serial port and returning true when a sequence is seen
     SoftwareSerial *serialPort;
     byte msgStartFlag[2] = {0xA0, 0xA1},
         msgEndFlag[2] = {0x0D, 0x0A};
@@ -53,31 +53,31 @@ private:
 
 
 public:
-    Hermes (SoftwareSerial *serial);    //ctor, sets the member software serial pointer to the ctor argument
+    Hermes (SoftwareSerial *serial);            //ctor, sets the member software serial pointer to the ctor argument
 
-    bool readSentence();            //read a normal gps sentence
-    void begin(int baud);       //set software serial port baud rate
-    void getResponse(char *buffer);     //return
-    void setRunMode(runMode mode);      //set whether or not to filter incoming data
-    void getSentence(char* buffer);     //fills the buffer with the last sentence seen
+    bool readSentence();                        //read a normal gps sentence
+    void begin(int baud);                       //set software serial port baud rate
+    void getResponse(char *buffer);             //return
+    void setRunMode(runMode mode);              //set whether or not to filter incoming data
+    void getSentence(char* buffer);             //fills the buffer with the last sentence seen
     void getField(char* buffer, int index);     //fills the buffer with whatever desired index
     bool getSoftwareVersion(char* buffer);      //fills the buffer with the software version
     bool setDefault();                          //sets the device the command to reset to factory defaults
     bool setSerialBaud(int speed);              //sends the gps the commands to configure it's baud rate
     bool setNmeaContent(int gga, int gsa, int gsv, int gll, int rmc, int vtg, int zda);     //configures what type of NMEA sentences are reported
-    bool setMsgType(int type);      //turns ff the nmea output or sets it to normal and binary mode.
-    bool setPowerMode(int mode);    //sets the power mode on the gps
-    bool setPositionUpdateRate(int rate);   //sets the position update rate and sets the baud rate to appropriate baud for safety
-    bool setWaas(bool enable);      //I don't really know what WAAS is, but it's easy to configure and sounded like it could be a useful setting
-    bool getWaas(char* buffer);     //fills the buffer with the WAAS status
+    bool setMsgType(int type);                  //turns ff the nmea output or sets it to normal and binary mode.
+    bool setPowerMode(int mode);                //sets the power mode on the gps
+    bool setPositionUpdateRate(int rate);       //sets the position update rate and sets the baud rate to appropriate baud for safety
+    bool setWaas(bool enable);                  //I don't really know what WAAS is, but it's easy to configure and sounded like it could be a useful setting
+    bool getWaas(char* buffer);                 //fills the buffer with the WAAS status
     bool setSaveMode(bool permanent = false);   //sets whether or not configuration of the gps is permanent. I recommend against unless you know you won't break the board.
-    bool getSaveMode();     //returns the saveMode member variable, in case you need it
-    bool setPositionPinning(bool enable);   //configures position pinning
+    bool getSaveMode();                         //returns the saveMode member variable, in case you need it
+    bool setPositionPinning(bool enable);       //configures position pinning
     bool getPositionPinning(char* buffer);      //fills the buffer with the pinning state
     bool configPositionPinning(unsigned int pinSpeed, unsigned int pinCnt, unsigned int unPinSpeed, unsigned int unPinCnt, unsigned int unPinDistance);     //changes position pinning settings on the gps board
-    bool setNavMode(bool mode);     //sets the mode to either false for car, or true for pedestrian
-    bool getNavMode(char* buffer);  //fills the buffer with the navigation mode
-    bool setPps(int mode);      //sets the pps mode from 0 off, 1 on when 3D fix, and 2 on when 1 SV
-    bool getPps(int* mode);     //sets the mode argument to whatever the GPS mode is
+    bool setNavMode(bool mode);                 //sets the mode to either false for car, or true for pedestrian
+    bool getNavMode(char* buffer);              //fills the buffer with the navigation mode
+    bool setPps(int mode);                      //sets the pps mode from 0 off, 1 on when 3D fix, and 2 on when 1 SV
+    bool getPps(int* mode);                     //sets the mode argument to whatever the GPS mode is
 };
 #endif
