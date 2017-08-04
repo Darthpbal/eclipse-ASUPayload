f110f2a5bba964dc532045d307f71898154849dd
darthpbal <Ronquillo.paul@gmail.com>
2017-08-04 05:28:13 -0700

    changed slaves to tested working versions

--- launchDay/payload/i2cSlave/altimeter_Slave/altimeter_Slave.ino
+++ launchDay/payload/i2cSlave/altimeter_Slave/altimeter_Slave.ino
@@ -1,49 +1,260 @@
+
+#include <Wire.h>
 #include <Adafruit_MPL3115A2.h>
-int cs = 8;
+
+// Power by connecting Vin to 3-5V, GND to GND
+// Uses I2C - connect SCL to the SCL pin, SDA to SDA pin
+// See the Wire tutorial for pinouts for each Arduino
+// http://arduino.cc/en/reference/wire
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
-
-
-float tempC;
-float altm;
-float pascals;
-
-//The Arduino Map function but for floats
-//From: http://forum.arduino.cc/index.php?topic=3922.0
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-{
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-
+const bool on = LOW, off = HIGH;
 void setup() {
-    pinMode(13, OUTPUT);
-    digitalWrite(13, HIGH);
-    Serial.begin(9600); //turn on serial monitor
-    baro.begin();   //initialize pressure sensor mySensor
-    pinMode(cs, INPUT);
-    digitalWrite(13, LOW);
+  Serial.begin(9600);
+  pinMode(8, INPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-    float pascals = baro.getPressure();
-    float altm = baro.getAltitude();
-    float tempC = baro.getTemperature();
+
+  if (! baro.begin()) {
+    Serial.println("Couldnt find sensor");
+    return;
+  }
+
+  float pascals = baro.getPressure();
+  float altm = baro.getAltitude();
+  float tempC = baro.getTemperature();
 
 
+  bool selState = digitalRead(8);
+  if(selState == on){
+    digitalWrite(13, HIGH);
 
-    if(digitalRead(cs)){
-        digitalWrite(13, HIGH);
+    Serial.print(pascals/101325); Serial.print(" atm,\t");
+    Serial.print(altm); Serial.print(" meters,\t");
+    Serial.print( ( (tempC * 9) / 5) + 32 ); Serial.print(" *F ");
+    Serial.println();
 
-        Serial.print(tempC);
-        Serial.print(F(","));
-        Serial.print(pascals);
-        Serial.print(F(","));
-        Serial.println(altm);
+    while(!digitalRead(8));
 
-        while(digitalRead(cs));
-        digitalWrite(13, LOW);
-    }
-
-
+    digitalWrite(13, LOW);
+  }
 
 }
+
+
+
+
+
+/*
+
+looks like test results are good.
+
+The sensor says that the temp readout is 72.72
+degrees F and I have a thermometer in my bedroom
+where I'm testing this sensor and it also reads 72
+degrees. While I can't confirm the altitude yet
+without running alongside the GPS, but a quick
+conversion from pascals to atm almost hits 1.0,
+which means it's almost right on.
+
+
+
+97494.00 pascals  0.96 atm  324.06 meters 72.72 *F
+97491.75 pascals  0.96 atm  323.94 meters 72.72 *F
+97496.00 pascals  0.96 atm  323.81 meters 72.72 *F
+97494.50 pascals  0.96 atm  323.81 meters 72.84 *F
+97493.50 pascals  0.96 atm  324.00 meters 72.72 *F
+97495.25 pascals  0.96 atm  324.00 meters 72.72 *F
+97494.75 pascals  0.96 atm  324.13 meters 72.72 *F
+97494.25 pascals  0.96 atm  324.19 meters 72.72 *F
+97495.75 pascals  0.96 atm  324.00 meters 72.84 *F
+97492.75 pascals  0.96 atm  323.81 meters 72.84 *F
+97492.75 pascals  0.96 atm  324.06 meters 72.84 *F
+97492.75 pascals  0.96 atm  323.94 meters 72.84 *F
+97496.25 pascals  0.96 atm  324.00 meters 72.84 *F
+
+
+
+
+
+
+
+
+
+arduino due test results 7/3/17
+
+47109.50 pascals  0.46 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+47483.25 pascals  0.47 atm  11777.37 meters 93.20 *F
+
+
+
+
+arduino due original code test results 7/3/17
+
+47109.50 pascals
+13.95 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+47483.25 pascals
+14.06 Inches (Hg)
+11777.37 meters
+34.00*C
+
+
+
+
+
+
+test on UNO 7/3/17
+
+
+97085.50 pascals  0.96 atm  358.94 meters 72.39 *F
+97084.75 pascals  0.96 atm  359.06 meters 72.39 *F
+97086.50 pascals  0.96 atm  359.13 meters 72.39 *F
+97086.25 pascals  0.96 atm  359.13 meters 72.39 *F
+97087.25 pascals  0.96 atm  359.13 meters 72.39 *F
+97087.50 pascals  0.96 atm  359.38 meters 72.39 *F
+97086.00 pascals  0.96 atm  359.25 meters 72.39 *F
+97082.50 pascals  0.96 atm  358.94 meters 72.39 *F
+97085.25 pascals  0.96 atm  358.94 meters 72.39 *F
+97085.00 pascals  0.96 atm  359.06 meters 72.39 *F
+97086.25 pascals  0.96 atm  359.31 meters 72.39 *F
+97087.25 pascals  0.96 atm  359.19 meters 72.39 *F
+97082.00 pascals  0.96 atm  359.13 meters 72.27 *F
+
+
+1434.75 pascals
+0.42 Inches (Hg)
+358.88 meters
+22.62*C
+
+97092.75 pascals
+28.75 Inches (Hg)
+358.50 meters
+22.69*C
+
+97088.75 pascals
+28.75 Inches (Hg)
+358.63 meters
+22.69*C
+
+97091.25 pascals
+28.75 Inches (Hg)
+358.63 meters
+22.69*C
+
+97092.75 pascals
+28.75 Inches (Hg)
+358.63 meters
+22.62*C
+
+97088.50 pascals
+28.75 Inches (Hg)
+358.81 meters
+22.62*C
+
+97090.00 pascals
+28.75 Inches (Hg)
+358.94 meters
+22.62*C
+
+97092.25 pascals
+28.75 Inches (Hg)
+358.69 meters
+22.62*C
+
+97091.25 pascals
+28.75 Inches (Hg)
+358.94 meters
+22.62*C
+
+*/
--- launchDay/payload/i2cSlave/optic_Slave/optic_Slave.ino
+++ launchDay/payload/i2cSlave/optic_Slave/optic_Slave.ino
@@ -1,107 +1,195 @@
+unsigned long tim = 0;
+  unsigned int data0, data1;
+  bool readCheck = false;
+#include <SparkFunTSL2561.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_TSL2561_U.h>
+const bool on = LOW, off = HIGH;
+// Create an SFE_TSL2561 object, here called "light":
 
-Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+SFE_TSL2561 light;
 
-int cs = 8;
+// Global variables:
 
+boolean gain;     // Gain setting, 0 = X1, 1 = X16;
+unsigned int ms;  // Integration ("shutter") time in milliseconds
 
-
-
-int UVOUT = A0; //Output from the sensor
-int REF_3V3 = A1; //3.3V power on the Arduino board
-
-
-int averageAnalogRead(int pinToRead)
+void setup()
 {
-  byte numberOfReadings = 8;
-  unsigned int runningValue = 0;
+  // Initialize the Serial port:
 
-  for(int x = 0 ; x < numberOfReadings ; x++)
-    runningValue += analogRead(pinToRead);
-  runningValue /= numberOfReadings;
-
-  return(runningValue);
-}
-
-//The Arduino Map function but for floats
-//From: http://forum.arduino.cc/index.php?topic=3922.0
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-void setup(void)
-{
-    pinMode(13, OUTPUT);
-    digitalWrite(13, HIGH);
-    pinMode(cs, INPUT);
   Serial.begin(9600);
+  pinMode(8, INPUT);
+  pinMode(13, OUTPUT);
 
-  /* Initialise the sensor */
-  if(!tsl.begin())
+  light.begin();
+
+  // Get factory ID from sensor:
+  // (Just for fun, you don't need to do this to operate the sensor)
+
+  unsigned char ID;
+
+  if (light.getID(ID))
   {
-    /* There was a problem detecting the TSL2561 ... check your connections */
-    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+    Serial.print("Got factory ID: 0X");
+    Serial.print(ID,HEX);
+    Serial.println(", should be 0X5X");
+  }
+  // Most library commands will return true if communications was successful,
+  // and false if there was a problem. You can ignore this returned value,
+  // or check whether a command worked correctly and retrieve an error code:
+  else
+  {
+    byte error = light.getError();
+    printError(error);
   }
 
-  /* Display some basic information on this sensor */
-//  displaySensorDetails();
+  // The light sensor has a default integration time of 402ms,
+  // and a default gain of low (1X).
 
-  /* Setup the sensor gain and integration time */
-//  configureSensor();
+  // If you would like to change either of these, you can
+  // do so using the setTiming() command.
 
-  /* We're ready to go! */
-  digitalWrite(13, LOW);
+  // If gain = false (0), device is set to low gain (1X)
+  // If gain = high (1), device is set to high gain (16X)
+
+  gain = 0;
+
+  // If time = 0, integration will be 13.7ms
+  // If time = 1, integration will be 101ms
+  // If time = 2, integration will be 402ms
+  // If time = 3, use manual start / stop to perform your own integration
+
+  unsigned char time = 2;
+
+  // setTiming() will set the third parameter (ms) to the
+  // requested integration time in ms (this will be useful later):
+
+//  Serial.println("Set timing...");
+  light.setTiming(gain,time,ms);
+
+  // To start taking measurements, power up the sensor:
+
+//  Serial.println("Powerup...");
+  light.setPowerUp();
+
+  // The sensor will now gather light during the integration time.
+  // After the specified time, you can retrieve the result from the sensor.
+  // Once a measurement occurs, another integration period will start.
 }
 
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
-void loop(void)
+void loop()
 {
+  // Wait between measurements before retrieving the result
+  // (You can also configure the sensor to issue an interrupt
+  // when measurements are complete)
 
-    int uvLevel = averageAnalogRead(UVOUT);
-    int refLevel = averageAnalogRead(REF_3V3);
+  // This sketch uses the TSL2561's built-in integration timer.
+  // You can also perform your own manual integration timing
+  // by setting "time" to 3 (manual) in setTiming(),
+  // then performing a manualStart() and a manualStop() as in the below
+  // commented statements:
 
-    //Use the 3.3V power pin as a reference to get a very accurate output value from sensor
-    float outputVoltage = 3.3 / refLevel * uvLevel;
+  // ms = 1000;
+  // light.manualStart();
+//  delay(ms);
+  // light.manualStop();
 
-    float uvIntensity = mapfloat(outputVoltage, 0.99, 2.9, 0.0, 15.0);
+  // Once integration is complete, we'll retrieve the data.
 
+  // There are two light sensors on the device, one for visible light
+  // and one for infrared. Both sensors are needed for lux calculations.
 
-
-
-    /* Get a new sensor event */
-    sensors_event_t event;
-    tsl.getEvent(&event);
-
-
-
-
-
-
-
-
-    if(digitalRead(cs)){
-        digitalWrite(13, HIGH);
-
-        /* Display the results (light is measured in lux) */
-//        if (event.light) 
-        Serial.print(event.light);
-//        else Serial.print("Sensor overload");
-
-        Serial.print(F(","));
-        Serial.println(uvIntensity);
+  // Retrieve the data from the device:
 
 
 
-        while(digitalRead(cs)) Serial.println(digitalRead(cs));
-        digitalWrite(13, LOW);
-    }
+
+if(millis() - tim > ms){
+  readCheck = light.getData(data0,data1);
+  tim = millis();
 }
+
+
+  bool selState = digitalRead(8);
+  if(selState == on){
+    digitalWrite(13, HIGH);
+
+
+  if (readCheck)
+  {
+
+    Serial.print("data0: ");
+    Serial.print(data0);
+    Serial.print(" data1: ");
+    Serial.print(data1);
+
+    double lux;    // Resulting lux value
+    boolean good;  // True if neither sensor is saturated
+
+    good = light.getLux(gain,ms,data0,data1,lux);
+
+    Serial.print(" lux: ");
+    Serial.print(lux);
+    if (good) Serial.println(" (good)"); else Serial.println(" (BAD)");
+  }
+  else
+  {
+    byte error = light.getError();
+    printError(error);
+  }
+
+
+    while(!digitalRead(8));
+
+    digitalWrite(13, LOW);
+  }
+
+
+}
+
+void printError(byte error)
+  // If there's an I2C error, this function will
+  // print out an explanation.
+{
+  Serial.print("I2C error: ");
+  Serial.print(error,DEC);
+  Serial.print(", ");
+
+  switch(error)
+  {
+    case 0:
+      Serial.println("success");
+      break;
+    case 1:
+      Serial.println("data too long for transmit buffer");
+      break;
+    case 2:
+      Serial.println("received NACK on address (disconnected?)");
+      break;
+    case 3:
+      Serial.println("received NACK on data");
+      break;
+    case 4:
+      Serial.println("other error");
+      break;
+    default:
+      Serial.println("unknown error");
+  }
+}
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+*/
